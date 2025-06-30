4759aa96e27d9551d8a69671758b7f145900aef9
shahriar nafis <snafaro02@gmail.com>
2025-06-30 12:10:20 +0600

    category button display

--- script/video.js
+++ script/video.js
@@ -1,1 +1,16 @@
-console.log('PH tube is here');
+const getCategory = () =>{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+}
+const displayCategories = (categories) =>{
+    categories.forEach(element => {
+        const buttonContainer = document.getElementById('categoryContainer');
+        const button = document.createElement('button');
+        button.innerText = element.category;
+        button.classList = "btn bg-[red] text-[white]";
+        buttonContainer.append(button);
+    });
+}
+getCategory();
